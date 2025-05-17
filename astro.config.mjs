fe4806544af0afe6ad002eb641d770d618a87da6
Bruno RASSAT <brunorassat@hotmail.fr>
2025-05-17 13:13:22 +0700

    refactor: reorganize imports and improve formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,38 +8,61 @@ import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from "rehype-external-links";
 import fauxRemarkEmbedder from "@remark-embedder/core";
 import fauxOembedTransformer from "@remark-embedder/transformer-oembed";
+import vue from "@astrojs/vue";
+import react from "@astrojs/react";
+import path from "path"; // <-- Required to resolve file system paths
+
 const remarkEmbedder = fauxRemarkEmbedder.default;
 const oembedTransformer = fauxOembedTransformer.default;
-import vue from "@astrojs/vue";
+
 /** @type {import('astro-m2dx').Options} */
-import react from "@astrojs/react";
 const m2dxOptions = {
   exportComponents: true,
   unwrapImages: true,
   autoImports: true
 };
 
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://brunorassat-portfolio.netlify.app",
-  integrations: [icon(), mdx({}), sitemap(), tailwind(), vue({
-    appEntrypoint: "/src/pages/_app"
-  }), astroImageTools, react()],
+  integrations: [
+    icon(),
+    mdx({}),
+    sitemap(),
+    tailwind(),
+    vue({
+      appEntrypoint: "/src/pages/_app"
+    }),
+    astroImageTools,
+    react()
+  ],
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [[remarkEmbedder, {
-      transformers: [oembedTransformer]
-    }], [m2dx, m2dxOptions]],
-    rehypePlugins: [[rehypeExternalLinks, {
-      rel: ["nofollow"],
-      target: ["_blank"]
-    }]]
+    remarkPlugins: [
+      [remarkEmbedder, {
+        transformers: [oembedTransformer]
+      }],
+      [m2dx, m2dxOptions]
+    ],
+    rehypePlugins: [
+      [rehypeExternalLinks, {
+        rel: ["nofollow"],
+        target: ["_blank"]
+      }]
+    ]
   },
   vite: {
+    resolve: {
+      alias: {
+        "@util": path.resolve("./src/util") // <-- Alias fix here
+      }
+    },
     build: {
       rollupOptions: {
-        external: ["/_pagefind/pagefind.js", "/_pagefind/pagefind-ui.js", "/_pagefind/pagefind-ui.css"]
+        external: [
+          "/_pagefind/pagefind.js",
+          "/_pagefind/pagefind-ui.js",
+          "/_pagefind/pagefind-ui.css"
+        ]
       },
       assetsInlineLimit: 10096
     }
